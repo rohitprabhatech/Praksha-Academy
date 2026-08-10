31e5ffbbd936988d155a05a7a2a1b7a409036ce3
AdityaKshirsagar-Prabha <aditya.kshirsagar.prabha@gmail.com>
2026-08-10 11:58:27 +0530

    feat(courses): complete courses module

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import Home from '../pages/Home'
 import Courses from '../pages/Courses'
+import CourseDetails from '../pages/CourseDetails'
 import Programs from '../pages/Programs'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
@@ -13,6 +14,7 @@ function AppRoutes() {
    <Route element={<MainLayout />}>
     <Route path="/" element={<Home />} />
     <Route path="/courses" element={<Courses />} />
+    <Route path="/courses/:slug" element={<CourseDetails />} />
     <Route path="/programs" element={<Programs />} />
     <Route path="/about" element={<About />} />
     <Route path="/contact" element={<Contact />} />
